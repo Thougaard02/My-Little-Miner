37890d7142548ac19a01a55e7cbc9dc47bad9d7d
Rasmus Thougaard <rasmusvinperlen@outlook.dk>
2020-10-19 15:11:54 +0200

    nothign changed

--- My Little Miner/Menu.cs
+++ My Little Miner/Menu.cs
@@ -10,26 +10,5 @@ namespace My_Little_Miner
     class Menu
     {
 
-        public void MainMenu()
-        {
-            Pickaxe pickaxe = new Pickaxe(PickaxeVariety.Wood);
-            Player player = new Player("Rasmus", true, pickaxe);
-            Mine game = new Mine();
-
-
-            lort();
-            int userinput = Convert.ToInt32(Console.ReadLine());
-            Console.Clear();
-
-        }
-
-        public void lort()
-        {
-            Console.WriteLine("My Little Miner");
-            Console.WriteLine("1. Go Mine");
-            Console.WriteLine("2. Open inventory");
-            Console.WriteLine("3. Smeltery");
-            Console.WriteLine("4. Go to shop");
-        }
     }
 }
