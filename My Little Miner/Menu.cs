9029c649f846a4a833524c5b1044131038f68bf7
Rasmus Thougaard <rasmusvinperlen@outlook.dk>
2020-10-19 14:27:21 +0200

    Creating GUI

---
+++ My Little Miner/Menu.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace My_Little_Miner
+{
+    class Menu
+    {
+
+        public void MainMenu()
+        {
+            Pickaxe pickaxe = new Pickaxe(PickaxeVariety.Wood);
+            Player player = new Player("Rasmus", true, pickaxe);
+            Mine game = new Mine();
+
+
+            lort();
+            int userinput = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+        }
+
+        public void lort()
+        {
+            Console.WriteLine("My Little Miner");
+            Console.WriteLine("1. Go Mine");
+            Console.WriteLine("2. Open inventory");
+            Console.WriteLine("3. Smeltery");
+            Console.WriteLine("4. Go to shop");
+        }
+    }
+}
--- My Little Miner/Program.cs
+++ My Little Miner/Program.cs
@@ -11,16 +11,9 @@ namespace My_Little_Miner
     {
         static void Main(string[] args)
         {
-            Pickaxe pickaxe = new Pickaxe(PickaxeVariety.Wood);
-            Player player = new Player("Rasmus", true, pickaxe);
-            Mine game = new Mine();
-
-            while (true)
-            {
-                Console.WriteLine("You got a new mineral: " + game.GoMining(player));                                            
-                Console.ReadKey();
-                Console.Clear();
-            }                   
+           
+            Menu menu = new Menu();
+            menu.MainMenu();                           
         }
     }
 }
